820b478d7d37447148b5e0b045a89e31bc823502
Tomi Tarvainen <ot_tomi@hotmail.com>
2024-02-04 20:21:44 +0200

    Add Bootstrap

--- hummingfront.client/src/App.vue
+++ hummingfront.client/src/App.vue
@@ -5,15 +5,23 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    Humming hummings
   </header>
 
   <main>
-    <TheWelcome />
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
   </main>
 </template>
 
