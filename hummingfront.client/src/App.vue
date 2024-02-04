7f7d8e8516197cd569274550ac6c0332f3f11016
Tomi Tarvainen <ot_tomi@hotmail.com>
2024-02-04 21:21:42 +0200

    Add Navbar

--- hummingfront.client/src/App.vue
+++ hummingfront.client/src/App.vue
@@ -5,6 +5,54 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <div>
+    <!-- navbar -->
+    <!-- create a component!!! -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Etusivu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Uutiset</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Kissat</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Naaraat</a></li>
+                        <li><a class="dropdown-item" href="#">Urokset</a></li>
+                        <li><a class="dropdown-item" href="#">Kastraatit</a></li>
+                        <li><a class="dropdown-item" href="#">Muistoissa</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pentukori</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pentueet</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">1. pentue</a></li>
+                        <li><a class="dropdown-item" href="#">2. pentue</a></li>
+                        <li><a class="dropdown-item" href="#">3. pentue</a></li>
+                        <li><a class="dropdown-item" href="#">4. pentue</a></li>
+                        <li><a class="dropdown-item" href="#">5. pentue</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Kissala</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Kasvattaja</a></li>
+                        <li><a class="dropdown-item" href="#">Pentu-info</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
 
     <header>
       <h1>Humming hummings</h1>
