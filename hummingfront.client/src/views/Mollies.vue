da7ac2356840c560cba37ec0e0c237b93a5a7faa
Tomi Tarvainen <ot_tomi@hotmail.com>
2024-02-05 21:35:04 +0200

    Add Mollies page

---
+++ hummingfront.client/src/views/Mollies.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <!-- <h1>TEST!</h1> -->
+        <div>
+            <p>
+                <p>SC Zhamanen Harlequin, DVM - Harppu</p>
+                <a title="SC Zhamanen Harlequin, DVM" href="https://web.archive.org/web/20130401081104/http://humming.nettisivu.org/naaraat/sc-fizhamanen-harlequin-dvm/">
+                    <img class="alignleft size-medium wp-image-16" title="SC FI*Zhamanen Harlequin, DVM" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\1609504678664.jpg" alt="Harppu" width="300" height="259">
+                </a>
+            </p>
+            <p>
+                SBI n 21
+                <br>
+                s. 28.7.2008
+            </p>
+            <p>
+                isä: EC FI*JoJon El Rayo Rumba (SBI n 21)
+                <br>
+                emo: IC FI*Zhamanen Ferrarina N.F.S. (SBI h)
+            </p>
+            <p>
+                Näyttelymenestys:
+                <span style="color: #339966;">20xBIV</span>,
+                <span style="color: #3366ff;">12xNOM</span> &amp;
+                <span style="color: #ff0000;">3xBIS</span>
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <p>
+                <p>SC Recknagel Tairapänks, DVM - Halka</p>
+                <a title="SC Recknagel Tairapänks, DVM" href="https://web.archive.org/web/20130401081104/http://humming.nettisivu.org/naaraat/sc-recknagel-tairapanks-dvm/">
+                    <img class="alignleft wp-image-17" title="SC FI*Recknagel Tairapänks" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20230901_192112.jpg" alt="Halka" width="273" height="373">
+                </a>
+            </p>
+            <p>
+                SBI a
+                <br>
+                s. 21.6.2009
+            </p>
+            <p>
+                isä: IC FI*Zhamanen Fangio, JW (SBI n)
+                <br>
+                emo: FI*Recknagel Fridakahlo (SBI b)
+            </p>
+            <p>
+                Näyttelymenestys:
+                <span style="color: #339966;">14xBIV</span>,
+                <span style="color: #3366ff;">19xNOM</span> &amp;
+                <span style="color: #ff0000;">2xBIS</span>
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <p>
+                <p>PengPeng :)</p>
+                <a title="" href="">
+                    <img class="alignleft wp-image-17" title="" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20230721_011959.jpg" alt="Hekla" width="273" height="373">
+                </a>
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <p>Hillevi :)</p>
+            <a title="" href="">
+                <img class="alignleft wp-image-17" title="" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20230817_160944.jpg" alt="Hillevi" width="273" height="373">
+            </a>
+        </div>
+
+        <hr>
+
+        <div>
+            <p>Hulda :)</p>
+            <a title="" href="">
+                <img class="alignleft wp-image-17" title="" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20231206_210954.jpg" alt="Hulda" width="273" height="373">
+            </a>
+        </div>
+  </div>
+</template>
