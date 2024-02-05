6a2f6142142a859e003c5fb0f5294d9402faddc0
Tomi Tarvainen <ot_tomi@hotmail.com>
2024-02-06 00:45:59 +0200

    Update Mollies page

--- hummingfront.client/src/views/Mollies.vue
+++ hummingfront.client/src/views/Mollies.vue
@@ -28,44 +28,6 @@
         <hr>
 
         <div>
-            <p>
-                <p>SC Recknagel Tairapänks, DVM - Halka</p>
-                <a title="SC Recknagel Tairapänks, DVM" href="https://web.archive.org/web/20130401081104/http://humming.nettisivu.org/naaraat/sc-recknagel-tairapanks-dvm/">
-                    <img class="alignleft wp-image-17" title="SC FI*Recknagel Tairapänks" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20230901_192112.jpg" alt="Halka" width="273" height="373">
-                </a>
-            </p>
-            <p>
-                SBI a
-                <br>
-                s. 21.6.2009
-            </p>
-            <p>
-                isä: IC FI*Zhamanen Fangio, JW (SBI n)
-                <br>
-                emo: FI*Recknagel Fridakahlo (SBI b)
-            </p>
-            <p>
-                Näyttelymenestys:
-                <span style="color: #339966;">14xBIV</span>,
-                <span style="color: #3366ff;">19xNOM</span> &amp;
-                <span style="color: #ff0000;">2xBIS</span>
-            </p>
-        </div>
-
-        <hr>
-
-        <div>
-            <p>
-                <p>PengPeng :)</p>
-                <a title="" href="">
-                    <img class="alignleft wp-image-17" title="" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20230721_011959.jpg" alt="Hekla" width="273" height="373">
-                </a>
-            </p>
-        </div>
-
-        <hr>
-
-        <div>
             <p>Hillevi :)</p>
             <a title="" href="">
                 <img class="alignleft wp-image-17" title="" src="C:\Users\Tomi\Coding\FullStack\HummingFront\hummingfront.client\src\assets\Images\20230817_160944.jpg" alt="Hillevi" width="273" height="373">
